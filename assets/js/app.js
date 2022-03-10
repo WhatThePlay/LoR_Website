d48be250da153f7c1d51cd3332610bcfc832e990
nicolas.dreier@students.ffhs.ch <nicolas.dreier@students.ffhs.ch>
2022-03-10 13:19:51 +0100

    keywords responsive first draft. First example using pokeapi in footer. other minor changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
 const menuButton = document.getElementById("menu")
 const nav = document.getElementById("nav")
+const headerHeight = document.getElementById("app").clientHeight
+const url = "https://pokeapi.co/api/v2/pokemon/"
 
 let menuIsOpen = false
 
 menuButton.addEventListener("click", () => {
     menuIsOpen = !menuIsOpen
 
-    if(menuIsOpen) {
+    if (menuIsOpen) {
         menuButton.innerText = "✖"
         nav.classList.add("visible")
     } else {
@@ -15,8 +17,18 @@ menuButton.addEventListener("click", () => {
     }
 })
 
+document.getElementById("banner").style.marginTop = headerHeight.toString() + "px";
 
+async function fetchPokemon (url) {
+    let id = Math.floor(Math.random()*898);
+    let response = await fetch(url + id);
+    let data = await response.json();
+    const pokemon = document.createElement("p")
+    pokemon.innerText = data.name + " is a cool Pokemon btw."
+    document.getElementsByTagName("footer")[0].appendChild(pokemon)
+}
 
+fetchPokemon(url)
 
 
 
